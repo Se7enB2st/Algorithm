ff91488c312672de701bcf7033ff03639b82cede
Se7enB2st <ethanhe1992@gmail.com>
2016-04-15 21:29:31 -0400

    Update foolbar.rb

--- foolbar.rb
+++ foolbar.rb
@@ -1,9 +1,8 @@
-=begin 
- Foolbar code challenge for The Firehose Project:
- For all numbers that are divisible by three, the player should say "foo."
- For any number divisible by five, the player should say "bar."
- Numbers divisible by both become "Foobar."
- =end
+#Foolbar coding challenge
+#Rule 1: For all numbers that are divisible by three, the player should say "foo."
+#Rule 2: For any number divisible by five, the player should say "bar."
+#Rule 3: Numbers divisible by both become "Foobar."
+
 
 #prompt user for a number of pattern they want to see
 puts "How many digits of pattern you want to see?"
