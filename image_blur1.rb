ef7d71f6c260befc110855584d7980aa4075be17
Se7enB2st <ethanhe1992@gmail.com>
2016-05-03 19:54:45 -0400

    Create image_blur1.rb

---
+++ image_blur1.rb
@@ -0,0 +1,4 @@
+#The Firehose Project - Algorithm: Image Blur 1
+
+#Rule 1:Build a class that allows us to build a new image with the data we specify.
+#Rule 2:Make it possible for the class to output the image to the screen.
