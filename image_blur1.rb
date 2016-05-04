ad39aca582697736b2c8c1f4454f6cb918afbd50
Se7enB2st <ethanhe1992@gmail.com>
2016-05-03 21:34:52 -0400

    Update image_blur1.rb

--- image_blur1.rb
+++ image_blur1.rb
@@ -2,3 +2,25 @@
 
 #Rule 1:Build a class that allows us to build a new image with the data we specify.
 #Rule 2:Make it possible for the class to output the image to the screen.
+
+class Image
+  attr_accessor :data
+
+  def initialize(array)
+    @data = array
+  end
+
+  def output_image
+    @data.each do |row|
+      puts row.join
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0 ,0, 1],
+  [0, 0, 0, 0]
+])
+image.output_image
