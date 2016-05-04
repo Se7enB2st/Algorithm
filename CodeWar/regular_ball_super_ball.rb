5dbb59b188c6d1206682e7b768e62671b68ead68
Se7enB2st <ethanhe1992@gmail.com>
2016-05-04 17:06:36 -0400

    Create regular_ball_super_ball.rb

---
+++ CodeWar/regular_ball_super_ball.rb
@@ -0,0 +1,13 @@
+#Regular Ball Super Ball
+
+#Create a class Ball.
+#Ball objects should accept one argument for "ball type" when instantiated.
+#If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+class Ball
+	attr_accessor :ball_type
+ 
+ def initialize(ball_type = "regular")
+ 	@ball_type = ball_type
+ end 
+end
