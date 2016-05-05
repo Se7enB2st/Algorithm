92ab33e7414861a7f561dc2d034f5e34c186bc59
Se7enB2st <ethanhe1992@gmail.com>
2016-05-05 00:04:31 -0400

    Create removes_duplicates.rb

---
+++ CodeWar/removes_duplicates.rb
@@ -0,0 +1,5 @@
+# Define the unique method that removes duplicates
+
+def unique(integers)
+  integers & integers #Set intersection, Returns a new array containing elements common to the two arrays, excluding any duplicates.
+end
