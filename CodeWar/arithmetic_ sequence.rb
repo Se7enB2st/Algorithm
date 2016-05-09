90ed5ef532533386804042d1a4849bf48412bc5b
Se7enB2st <ethanhe1992@gmail.com>
2016-05-09 19:51:25 -0400

    Update arithmetic_ sequence.rb

--- CodeWar/arithmetic_ sequence.rb
+++ CodeWar/arithmetic_ sequence.rb
@@ -2,7 +2,7 @@
 #A sequence is usually a set or an array of numbers that has a strict way for moving from the nth term to the (n+1)th term.
 #If f(n) = f(n-1) + c where c is a constant value, then f is an arithmetic sequence.
 
-#You're going to write a function that's going to return the value in the nth index of an arithmetic sequence.
+#Rule: You're going to write a function that's going to return the value in the nth index of an arithmetic sequence.
 
 def nthterm(first, n, c)
   first + n * c
