5f334abb2fe260e1ca88d36dc6914c60ca5e3441
Se7enB2st <ethanhe1992@gmail.com>
2016-05-10 22:16:31 -0400

    Create get_planet_name_by_id.rb

---
+++ CodeWar/get_planet_name_by_id.rb
@@ -0,0 +1,22 @@
+#Get Planet Name By ID
+
+#The function is not returning the correct values. Can you figure out why?
+
+
+
+
+def get_planet_name(id)
+  
+
+  case id
+    when 1 then "Mercury"
+    when 2 then "Venus"
+    when 3 then "Earth"
+    when 4 then "Mars"
+    when 5 then"Jupiter"
+    when 6 then "Saturn"
+    when 7 then "Uranus"  
+    when 8 then "Neptune"
+  end
+  
+end
