1a6f33200d7e7a1f4f92d626496590aa769d93f8
Se7enB2st <ethanhe1992@gmail.com>
2016-05-13 13:51:08 -0400

    Create calculate_mean.rb

---
+++ CodeWar/calculate_mean.rb
@@ -0,0 +1,17 @@
+#Calculate Mean
+#Create a mathematical algorithm to calculate the mean of a series of numbers.
+#The mean of a series of numbers is the sum of all items in a set, divided by the number of items in that set.
+
+def calc_mean(ary)
+  sum = 0
+  if !ary.is_a?(Array)
+    0
+  elsif ary.empty?
+    0
+  else
+    ary.each do |i|
+      sum = sum + i
+    end
+  return sum / ary.count
+  end
+end
