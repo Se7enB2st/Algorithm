4e52112c64d98dd0d80f924ed174a4b75d79c69e
Se7enB2st <ethanhe1992@gmail.com>
2016-05-15 18:07:41 -0400

    Update image_blur2.rb

--- image_blur2.rb
+++ image_blur2.rb
@@ -4,7 +4,6 @@
 #Running the blur transformation causes any 1 in the image to cause the pixel to the left, right, above and below to become a 1.
 
 class Image
-  attr_accessor :image
 
   def initialize(image)
     @image = image
@@ -16,34 +15,30 @@ class Image
     end
   end
 
-  #Build a new image and return it
-  #So far, no transformation
-  def transform
-    one_pixel = []
-    @image.each_with_index do |row, row_index|
-      row.each_with_index do |pixel, col_index|
+  def blur
+    #Create a copy of image
+    @new_image = Marshal.load(Marshal.dump(@image))
+    #Loop through image, if find a pixel equal to 1 change new image
+    @image.each_with_index do |row, row_number|
+      row.each_with_index do |pixel, col_number |
         if pixel == 1
-
+    #Need explaining here ...
+          @new_image[row_number - 1][col_number] = 1 unless row_number == 0 #Up
+          @new_image[row_number + 1][col_number] = 1 unless (row_number + 1) >= @image.length #Down
+          @new_image[row_number][col_number - 1] = 1 unless col_number == 0 #Left
+          @new_image[row_number][col_number + 1] = 1 unless (col_number + 1) >= row.length #Right
         end
       end
     end
-
-    return Image.new(@image)
+    Image.new(@new_image)
   end
 end
 
 image = Image.new([
   [0, 0, 0, 0],
   [0, 1, 0, 0],
-  [0, 0 ,0, 1],
+  [0, 0, 0, 1],
   [0, 0, 0, 0]
-  ])
-  afterImage =  image.transform
-  afterImage.output_image
-  # after_transform = Image.new([
-  #    [0, 1, 0, 0],
-  #    [1, 1, 1, 1],
-  #    [0, 1 ,1, 1],
-  #    [0, 0, 0, 1]
-  #  ])
+])
+blurred_image = image.blur.output_image
 
