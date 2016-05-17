638518cfe26bd50d808b99dc6f6390b6f2330c0a
Se7enB2st <ethanhe1992@gmail.com>
2016-05-16 23:09:55 -0400

    Create image_blur3.rb

---
+++ image_blur3.rb
@@ -0,0 +1,8 @@
+#The Firehose Project Algorithm Challenge: Image_Blur#3
+
+#Given an image, we want to build a method to blur the image, however instead of only blurring images that are within 1 pixel,
+# we want to specify how far to blur pixels that are within a Manhattan Distance of what is specified. 
+
+def blur(distance)
+
+end
