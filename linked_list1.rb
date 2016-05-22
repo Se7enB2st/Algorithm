6e6ce4c1348862029785e38ca2474d5a717e3aba
Ethan He <ethanhe1992@gmail.com>
2016-05-22 17:28:39 -0400

    Create linked_list1.rb

---
+++ linked_list1.rb
@@ -0,0 +1,25 @@
+#The Firehose Project - Algorithm Problem: Linked List 1
+
+#A LinkedList is a node that has a specific value attached to it (which is sometimes called a payload),
+#and a link to another node (or nil if there is no next item).
+
+class Stack
+    attr_reader :data
+    
+    def initialize
+        @data = nil
+    end
+
+    # Push a value onto the stack
+    def push(value)
+        # IMPLEMENT ME!
+    end
+
+    # Pop an item off the stack.  
+    # Remove the last item that was pushed onto the
+    # stack and return the value to the user
+    def pop
+        # I RETURN A VALUE
+    end
+
+end
