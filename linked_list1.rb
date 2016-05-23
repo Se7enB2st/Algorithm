d74db89b7a9b3ef7c42076f1feee3f3bc95d5f92
Ethan He <ethanhe1992@gmail.com>
2016-05-22 21:30:25 -0400

    Update linked_list1.rb

--- linked_list1.rb
+++ linked_list1.rb
@@ -3,9 +3,52 @@
 #A LinkedList is a node that has a specific value attached to it (which is sometimes called a payload),
 #and a link to another node (or nil if there is no next item).
 
+class LinkedListNode
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node=nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  # node3.print_value
+  def print_values
+    print "#{self.value} --> "
+    if self.next_node != nil
+      self.next_node.print_values
+    else
+      print "nil\n"
+    end
+  end
+
+end
+
+# print_values(node3)
+def print_values(list_node)
+  if list_node
+    print "#{list_node.value} --> "
+    print_values(list_node.next_node)
+  else
+    print "nil\n"
+    return
+  end
+end
+
+def reverse_list(list)
+  # ADD CODE HERE
+
+  while list
+      # ADD CODE HERE
+      list = list.next_node
+  end
+
+  # ADD CODE HERE
+end
+
+
 class Stack
     attr_reader :data
-    
+
     def initialize
         @data = nil
     end
@@ -15,7 +58,7 @@ class Stack
         # IMPLEMENT ME!
     end
 
-    # Pop an item off the stack.  
+    # Pop an item off the stack.
     # Remove the last item that was pushed onto the
     # stack and return the value to the user
     def pop
@@ -23,3 +66,16 @@ class Stack
     end
 
 end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+node3.print_values(node3)
+
+puts "-------"
+
+revlist = reverse_list(node3)
+
+print_values(revlist)
+
