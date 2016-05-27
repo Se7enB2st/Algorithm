b022db61e55c9ef0ad33d81b98bc0a99aaf7d07e
Ethan He <ethanhe1992@gmail.com>
2016-05-27 16:50:47 -0400

    Create hello_world.rb

---
+++ CodeWar/hello_world.rb
@@ -0,0 +1,10 @@
+#Classic Hello World
+
+#The main mode is having a method named main inside a class and should return nothing
+#but should print a line to the standard output with the message Hello World!.
+
+class Solution
+   def self.main(args)
+      puts "Hello World!"
+   end
+end
