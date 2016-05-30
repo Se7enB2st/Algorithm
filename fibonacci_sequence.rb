a406baf0ce10d4802fa9f5906ff1e34fa2f4757d
Ethan He <ethanhe1992@gmail.com>
2016-05-29 22:53:02 -0400

    Create fibonacci_sequence.rb

---
+++ fibonacci_sequence.rb
@@ -0,0 +1,9 @@
+#The Firehose Project - Algorithm Problem: Fibonacci Sequence
+
+#The technical mathematical definition of Fibonacci numbers is below:
+#fib(0) = 0 
+#fib(1) = 1 
+#fib(n) = fib(n-1) + fib(n-2)
+
+#Build a method called iterative_fib that builds a table of Fibonacci numbers one-by-one like we walked you through earlier.
+#After you build the entire sequence of numbers up to n digits, just return the last value.
