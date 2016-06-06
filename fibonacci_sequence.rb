1dde544a689befe37146554dd298287554ba0ac9
Ethan He <ethanhe1992@gmail.com>
2016-06-05 23:42:09 -0400

    Update fibonacci_sequence.rb

--- fibonacci_sequence.rb
+++ fibonacci_sequence.rb
@@ -7,3 +7,17 @@
 
 #Build a method called iterative_fib that builds a table of Fibonacci numbers one-by-one like we walked you through earlier.
 #After you build the entire sequence of numbers up to n digits, just return the last value.
+
+#Iterative method
+def iterative_fib(num)
+  array = [0,1]
+  while array.length != num-1
+    x = array[array.length-1]
+    y = array[array.length-2]
+    result = x + y
+    array << result
+  end
+  return array.last
+end
+
+puts iterative_fib(10)
