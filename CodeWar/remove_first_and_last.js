5a96d87ee7d4d2bd3b4d0be895bf49bd641408f5
Ethan He <ethanhe1992@gmail.com>
2016-06-15 19:45:22 -0400

    Create remove_first_and_last.js

---
+++ CodeWar/remove_first_and_last.js
@@ -0,0 +1,7 @@
+//Remove First and Last Character - Codewars
+
+//Rule: Create a function that removes the first and last characters of a string. You're given one parameter.
+
+function removeChar(str){
+  return str.slice(1, -1);
+};
