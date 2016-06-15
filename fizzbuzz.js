e3229136597b521ded6314fc98531d51948ad8ea
Ethan He <ethanhe1992@gmail.com>
2016-06-15 00:04:52 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,6 @@
-#FizzBuzz 
+//FizzBuzz algorithm problem:
+
+//Solution 1:
 #Rule: For numbers divisible by 3, print out "Fizz".
       #For numbers divisible by 5, print out "Buzz".
       #For numbers divisible by both 3 and 5, print out "FizzBuzz" in the console.
@@ -25,15 +27,15 @@ fizzBuzz(100);
 
 var readlineSync = require('readline-sync');
 
-var input = readlineSync.question('How many digit of FooBar shall I produce: ');
+var input = readlineSync.question('How many digit of FizzBuzz shall I produce: ');
 
 for (var iteration = 1; iteration <= Number(input); iteration++) {
   if (iteration % 3 === 0 && iteration % 5 === 0) {
-    console.log('FooBar');
+    console.log('FizzBuzz');
   } else if (iteration % 3 === 0) {
-    console.log('Foo');
+    console.log('Fizz');
   } else if (iteration % 5 === 0) {
-    console.log('Bar');
+    console.log('Buzz');
   } else {
     console.log(iteration);
   }
