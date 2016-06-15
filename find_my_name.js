f916e23855501203b3da499f0a880a5791523b89
Ethan He <ethanhe1992@gmail.com>
2016-06-14 23:26:14 -0400

    Create find_my_name.js

---
+++ find_my_name.js
@@ -0,0 +1,23 @@
+#Search Your Name 
+
+text = "Hello My name is Ethan, I love to code and build cool stuffs online."
+
+var myName = "Ethan";
+var hits = [];
+
+// Look for "E" in the text
+for(var i = 0; i < text.length; i++) {
+	if (text[i] === "E") {
+		// If we find it, add characters up to
+		// the length of my name to the array
+		for(var j = i; j < (myName.length + i); j++) {
+			hits.push(text[j]);
+		}
+	}
+}
+
+if (hits.length === 0) {
+	console.log("Your name wasn't found!");
+} else {
+	console.log(hits);
+}
