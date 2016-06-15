fee40abda1b180196aa039e1ac973cc97ade1217
Ethan He <ethanhe1992@gmail.com>
2016-06-14 22:32:57 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -15,7 +15,27 @@ var fizzBuzz = function(num) {
         } else {
             console.log(i);
         }
-    };
-};
+    }
+}
 
 fizzBuzz(100);
+
+//Solution 2:
+//Require NodeJS & Readline-Sync
+
+var readlineSync = require('readline-sync');
+
+var input = readlineSync.question('How many digit of FooBar shall I produce: ');
+
+for (var iteration = 1; iteration <= Number(input); iteration++) {
+  if (iteration % 3 === 0 && iteration % 5 === 0) {
+    console.log('FooBar');
+  } else if (iteration % 3 === 0) {
+    console.log('Foo');
+  } else if (iteration % 5 === 0) {
+    console.log('Bar');
+  } else {
+    console.log(iteration);
+  }
+}
+
