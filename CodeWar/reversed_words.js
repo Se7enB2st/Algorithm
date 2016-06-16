84ba060797eaa7f84a24fadc0f174de342ad4803
Ethan He <ethanhe1992@gmail.com>
2016-06-16 17:09:39 -0400

    Create reversed_words.js

---
+++ CodeWar/reversed_words.js
@@ -0,0 +1,7 @@
+//Reversed Words - Codewars
+
+//Create a function which take 1 string and reverses all of the words within the string passed in.
+
+function reverseWords(str){
+  return str.split(' ').reverse().join(' ');
+}
