45e07d814ad60beb0a073f3702ae2a56344e8932
Ethan He <ethanhe1992@gmail.com>
2016-06-16 00:22:51 -0400

    Update prime.js

--- CodeWar/prime.js
+++ CodeWar/prime.js
@@ -7,12 +7,12 @@
 function isPrime(num) {
    // If n is less than 2 or not an integer then by definition cannot be prime.
    if(num < 2 || !Number.isInteger(num)) return false
-   
+  
    // Now check every whole number from 2 to the square root of n. If any of these divides n exactly, n cannot be prime.
    for (var i = 2; i <= Math.sqrt(num); i++) {
       if (num % i == 0) return false;
    }
    
-	 // Finally return whether n is prime or not.
+   // Finally return whether n is prime or not.
    return true;
 }
