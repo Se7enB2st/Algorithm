7cb1a53cebed357a8a869b824db18427f6338a4d
Ethan He <ethanhe1992@gmail.com>
2016-06-17 22:21:21 -0400

    Update collatz.rb

--- collatz.rb
+++ collatz.rb
@@ -1,30 +1,53 @@
-#n → n/2 (n is even)
-#n → 3n + 1 (n is odd)
+#Find the input that produces the longest chain of collatz values from 1 to 1,000,000.
+#ex) n = 3, sequence: 3 → 10 → 5 → 16 → 8 → 4 → 2 → 1, length: 8
 
-def collatz(num) 
-  num_array = []
-  num_array.push[num]
+def collatz_sequence(range) #takes range and outputs number with greatest
+  #length of collatz sequence to reach 1
 
-  while num != 1
-    if num % 2 == 0
-      num = num/2
-      num_array.push[num]
-    else
-      num = 3*num+1
-      num_array.push(num)
+  range = range.to_s.gsub(/,/,'').to_i
+  return nil if range < 1
+
+  highest_length = 0
+  start_num = 0
+
+  (1..range).each do |n|
+
+    length = 1 #n is part of full length to reach 1
+    current_num = n
+
+    until current_num == 1
+      current_num = current_num.even? ? current_num / 2 : current_num * 3 + 1
+      length += 1
     end
+
+    next if highest_length > length
+    highest_length,start_num = length,n
+    #why is next method below not working?
+    #length > highest_l ? highest_l,start_n = length,n : next
   end
-  return num_array.length
+
+  p "#{start_num} produced the longest chain of collatz values with #{highest_length} length"
 end
 
-def counter(num1, num2)
-  counter_array = []
-  while num1 <= num2
-    n = collatz(num1)
-    num1 += 1
-    counter_array << n
+def collatz_seq(num,length=1) #recursive solution.
+  #Takes individual number(not range)
+  #and outputs length of terms took to reach 1
+  return length if num == 1
+
+  if num % 2 == 0
+    return collatz_seq(num / 2,length + 1)
+  else
+    return collatz_seq(3 * num + 1,length + 1)
   end
-  puts "#{counter_array.index(counter_array.max)+1} has the longest collatz sequence at #{counter_array.max}."
+
 end
 
-counter(1, 1000000)
+
+collatz_sequence("1,000,000") #837799 with 525 length
+collatz_sequence("100") #97 with 119 length
+
+#took range, passed individual n to recursive method to get total length of that n,
+#then find max length with mapped values.
+num_and_length = (1..1000000).map {|num| [num,collatz_seq(num)] }.max_by {|length| length[1]} #return [num,length]
+puts "#{num_and_length.shift} produced the longest chain of collatz values with #{num_and_length.shift} length"
+      #[837799, 525].shift = 837799                                             #[525].shift = 525
