b4d9b46258014c3c76ca03fcd8c6010a32041a05
Ethan He <ethanhe1992@gmail.com>
2016-06-19 18:13:15 -0400

    Update and rename linked_list1.rb to reverse_linked_list_stack.rb

--- reverse_linked_list_stack.rb
+++ reverse_linked_list_stack.rb
@@ -1,36 +1,36 @@
-#The Firehose Project - Algorithm Problem: Linked List 1
+#The Firehose Project - Algorithm Problem: Reverse a linked list using stack
 
 #A LinkedList is a node that has a specific value attached to it (which is sometimes called a payload),
 #and a link to another node (or nil if there is no next item).
 
 class LinkedListNode
-  attr_accessor :value, :next_node
+  attr_accessor :payload, :next_node
 
-  def initialize(value, next_node=nil)
-    @value = value
-    @next_node = next_node
+  def initialize(payload, next_node=nil)
+    self.payload = payload
+    self.next_node = next_node
   end
 end
 
-# data structure we are using to help us reverse the linked list
-class Stack
-  attr_reader :data
+# Stack data structure we are using to help us reverse the linked list
+class Stack #LIFO
+  attr_reader :list
 
   def initialize
-    @data = nil
+    @list = nil
   end
 
   # push a value onto the stack
   def push(value)
-    @data = LinkedListNode.new(value, @data)
+    @list = LinkedListNode.new(value, @list)
   end
 
   # pop top item off the stack and return the value to the user
   def pop
     # store top node value in variable
-    node_value = @data.value
+    node_value = @list.value
     # overwrite top node with next node in stack AKA pop top node off stack
-    @data = @data.next_node
+    @list = @list.next_node
     # return top node value
     return node_value
   end
@@ -39,7 +39,7 @@ end
 # recursive function prints node values in linked list
 def print_values(list_node)
   if list_node
-    print "#{list_node.value} --> "
+    print "#{list_node.payload} --> "
     print_values(list_node.next_node)
   else
     print "nil\n"
@@ -47,17 +47,17 @@ def print_values(list_node)
   end
 end
 
-def reverse_list(list)            # 12 -> 99 -> 37 -> nil
-  # create stack
+def reverse_list(original_list)            # 12 -> 99 -> 37 -> nil
+  # create a new stack
   stack = Stack.new
   # loop through linked list and push each node onto the stack
-  while list != nil
-    # push node on to stack
-    stack.push(list.value)
-    # jump to next node in original linked list
-    list = list.next_node
+  while original_list != nil
+    # push node from original list on to the new stack
+    stack.push(original_list.payload)
+    # set the next value in the original list to be next value to be push off
+    original_list = original_list.next_node
   end
-  stack.data
+  stack.list
 end
 
 # building 3 individual nodes and linking them to each other
