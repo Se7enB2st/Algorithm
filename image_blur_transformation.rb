4ff92faacdbb4ba7907c7e5c4eccd9c57e7069f7
Ethan He <ethanhe1992@gmail.com>
2016-06-19 23:37:09 -0400

    Update and rename image_blur2.rb to image_blur_transformation.rb

--- image_blur_transformation.rb
+++ image_blur_transformation.rb
@@ -1,32 +1,32 @@
-#The Firehose Project - Algorithm Problem: Image Blur 2
+#The Firehose Project - Algorithm Problem: Image Blur transformation
 
 #Given a black and white image, represented as the class you already implemented, implement a blur method to run a transformation on the image. 
 #Running the blur transformation causes any 1 in the image to cause the pixel to the left, right, above and below to become a 1.
 
 class Image
 
-  def initialize(image)
-    @image = image
+  def initialize(array)
+    @array = array
   end
 
   def output_image
-    @image.each do |row|
+    @array.each do |row|
       puts row.join
     end
   end
 
   def blur
-    #Create a copy of image
-    @new_image = Marshal.load(Marshal.dump(@image))
-    #Loop through image, find the "1 pixel"
-    @image.each_with_index do |row, row_number|
+    #Create a clone of image
+    @new_image = Marshal.load(Marshal.dump(@array))
+    #Loop through array, find the "1 pixel"
+    @array.each_with_index do |row, row_number|
       row.each_with_index do |pixel, col_number |
         if pixel == 1
-          #Blur the pixel that are top,down,left,right of the "1 Pixel" to 1
-          @new_image[row_number - 1][col_number] = 1 unless row_number == 0 #Top
-          @new_image[row_number + 1][col_number] = 1 unless (row_number + 1) >= @image.length #Down
-          @new_image[row_number][col_number - 1] = 1 unless col_number == 0 #Left
-          @new_image[row_number][col_number + 1] = 1 unless (col_number + 1) >= row.length #Right
+          #Blur the pixel that are on top, down, left, right of the "1 Pixel" to 1
+          @new_image[row_number - 1][col_number] = 1 unless row_number == 0 #Top / dont blur if at the top edge
+          @new_image[row_number + 1][col_number] = 1 unless (row_number + 1) >= @array.length #Down / dont blur if at bottom edge
+          @new_image[row_number][col_number - 1] = 1 unless col_number == 0 #Left / dont blur if at left edge
+          @new_image[row_number][col_number + 1] = 1 unless (col_number + 1) >= row.length #Right / dont blur if at right edge
         end
       end
     end
@@ -40,7 +40,13 @@ image = Image.new([
   [0, 0, 0, 1],
   [0, 0, 0, 0]
 ])
+
 blurred_image = image.blur.output_image
+#output = 0 1 0 0
+#         1 1 1 1
+#         0 1 1 1
+#         0 0 0 1
+
 
 #RSpec testing 
 require 'spec_helper'
