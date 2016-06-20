0f72d87914f0b96d3e5d81d5e7ab0773581dccf4
Ethan He <ethanhe1992@gmail.com>
2016-06-19 23:16:43 -0400

    Update and rename image_blur1.rb to image_blur_two_dimension.rb

--- image_blur_two_dimension.rb
+++ image_blur_two_dimension.rb
@@ -1,4 +1,4 @@
-#The Firehose Project - Algorithm Problem: Image Blur 1
+#The Firehose Project - Algorithm Problem: Create a new 2D Array (image)
 
 #Rule 1:Build a class that allows us to build a new image with the data we specify.
 #Rule 2:Make it possible for the class to output the image to the screen.
@@ -8,15 +8,15 @@
       # 0001
       # 0000
 
-class Image
-  attr_accessor :data
+class Image #Image class
+  attr_accessor :array
 
   def initialize(array)
-    @data = array
+    self.array = array
   end
 
-  def output_image
-    @data.each do |row|
+  def output_image #Create a new 2D array(image)
+    self.array.each do |row|
       puts row.join
     end
   end
@@ -28,4 +28,5 @@ image = Image.new([
   [0, 0 ,0, 1],
   [0, 0, 0, 0]
 ])
+
 image.output_image
