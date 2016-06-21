530d1b4b6de48d38afdb7bea53bdb4fe63632dfc
Ethan He <ethanhe1992@gmail.com>
2016-06-21 00:32:35 -0400

    Update and rename foolbar.rb to fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 #Foolbar coding challenge
-#Rule 1: For all numbers that are divisible by three, the player should say "foo."
-#Rule 2: For any number divisible by five, the player should say "bar."
-#Rule 3: Numbers divisible by both become "Foobar."
+#Rule 1: For all numbers that are divisible by three, the player should say "Fizz."
+#Rule 2: For any number divisible by five, the player should say "Buzz."
+#Rule 3: Numbers divisible by both become "FizzBuzz".
 
 
 #prompt user for a number of pattern they want to see
@@ -12,13 +12,13 @@ n = 0
 
 while n < digits
   n = n+1
-#foobar logic:    
+#fizzbuzz logic:    
     if n % 3 == 0 && n % 5 == 0
-        puts "foobar"
+        puts "FizzBuzz"
     elsif n % 3 == 0
-        puts "foo"
+        puts "Fizz"
     elsif n % 5 == 0 
-        puts "bar"
+        puts "Buzz"
     else
         puts "#{n}"
   end
@@ -29,6 +29,7 @@ def fizzbuzz(num)
   i = 0
   while i < num
   i += 1
+  
     if i % 3 == 0 && i % 5 == 0
       puts "FizzBuzz"
     elsif i % 5 == 0
