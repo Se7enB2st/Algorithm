06d80e2f832f8dcd504e85a7fc6ad2a6f8fa28c1
Ethan He <ethanhe1992@gmail.com>
2016-07-01 12:28:56 -0400

    Create balanced_parentheses.rb

---
+++ balanced_parentheses.rb
@@ -0,0 +1,25 @@
+#Given an expression in the form of a string that includes parantheses,
+#check if all opening parantheses are balanced(are all closed,closed in order).
+
+#Stack Approach:
+def balanced_parentheses(string)
+  strings = string.scan(/\W/) #only check non world characters
+  parentheses = { '{' => '}', '[' => ']', '(' => ')' } #match parentheses in hash
+  stack = []
+
+  strings.each do |s|
+    if parentheses.include? s #put parentheses into stack
+      stack.push(s)
+    elsif parentheses.values.include? s
+      if stack.empty? || stack.last != parentheses.invert[s] #return an inverted hash
+        return false
+      else
+        stack.pop
+      end
+    end
+  end
+
+  stack.empty? ? true : false #empty means all parentheses find their match else if it unbalanced
+end
+
+p balanced_parentheses("H[el(lo, e{very} one!)]") #true
