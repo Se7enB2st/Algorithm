20c42f3596204bb60c7440790c7989f038126070
Ethan He <ethanhe1992@gmail.com>
2016-08-01 23:49:46 -0400

    Update and rename area.rb to area_formulas.rb

--- area.rb
+++
@@ -1,5 +0,0 @@
-# Create a method find the area of circle given the radius
-
-def area_of_circle(radius)
-  Math::PI*(radius*radius)
-end
---
+++ area_formulas.rb
@@ -0,0 +1,18 @@
+# Area formulas in Ruby
+
+# Create a method that finds the area of circle given the radius
+def area_of_circle(radius)
+  Math::PI*(radius*radius)
+end
+
+# Create a method that finds the area of square given the length
+def area_of_square(length)
+	length*length
+end 
+
+# Create a method that finds the area of rectangle given the width and length
+def area_of_rectangle(width, length)
+	width*length
+end 
+
+
