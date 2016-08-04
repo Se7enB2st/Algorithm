6eb19d96fb69d94fe1846ba9b4f8a1558f09864c
Ethan He <ethanhe1992@gmail.com>
2016-08-04 16:27:42 -0400

    Update perimeter_formulas.rb

--- perimeter_formulas.rb
+++ perimeter_formulas.rb
@@ -9,3 +9,14 @@ end
 def perimeter_of_rectangle(length, width)
 	2*(length + width)
 end 
+
+# Permieter of Circle
+def perimeter_of_circle(radius)
+	2*Math::PI*radius
+end 
+
+# Permieter of Triangle
+def perimeter_of_triangle(side1, side2, base)
+	side1 + side2 + base 
+end 
+	
