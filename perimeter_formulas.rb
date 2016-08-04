1652c430a44897d537d361cb567d77992db50c86
Ethan He <ethanhe1992@gmail.com>
2016-08-04 16:22:26 -0400

    Create perimeter_formulas.rb

---
+++ perimeter_formulas.rb
@@ -0,0 +1,11 @@
+# Perimeter Formualas in Ruby
+
+# Permieter of Square
+def perimeter_of_square(side)
+	4*side
+end 
+
+# Permieter of Rectangle
+def perimeter_of_rectangle(length, width)
+	2*(length + width)
+end 
