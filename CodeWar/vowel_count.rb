8e20008501363cc4574e52cb03443ce1d5e5392a
Ethan He <ethanhe1992@gmail.com>
2016-11-15 22:50:17 -0500

    Create vowel_count.rb

---
+++ CodeWar/vowel_count.rb
@@ -0,0 +1,8 @@
+//Codewars Vowel Count
+
+//Return the number (count) of vowels in the given string.
+//We will consider a, e, i, o, and u as vowels for this Kata.
+
+def getCount(inputStr)
+ inputStr.downcase.count('aeiou')
+end
