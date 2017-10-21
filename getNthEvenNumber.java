d0ddb82776ee474abd300f01e8f0529059a310c8
Ethan He <ethanhe1992@gmail.com>
2017-10-21 00:47:31 -0400

    Create getNthEvenNumber.java

---
+++ getNthEvenNumber.java
@@ -0,0 +1,7 @@
+// Return the Nth Even Number
+// nthEven(1) //=> 0, the first even number is 0
+// nthEven(3) //=> 4, the 3rd even number is 4 (0, 2, 4)
+
+function nthEven(n) {
+  return 2 * (n - 1);
+}
