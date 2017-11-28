61a0d72c6f0d9ec837f7ab2ab8516b18765a2c59
Ethan He <ethanhe1992@gmail.com>
2017-11-28 12:19:11 -0500

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,3 +1,8 @@
+//The technical mathematical definition of Fibonacci numbers is below:
+//fib(0) = 0 
+//fib(1) = 1 
+//fib(n) = fib(n-1) + fib(n-2)
+
 public static long fibonacci(int n) {
   if(n == 0) {
     return 0;
